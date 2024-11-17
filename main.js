a6268d2c96808868079cee66a447e664207e2ccc
HarkiratDhill <Harkirat.Dhillon@edu.sait.ca>
2024-11-17 12:23:47 -0700

    Greetings and Button
    I have added in a button and a greeting for different hours to JavaScript but will change them at a later date.

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 const fYear = document.getElementById("end");
 const d = new Date();
-fYear.textContent = d.getFullYear("November 17");
+fYear.textContent = d.getFullYear();
 
 const sayHello = (name) => {
     console.log(`Hello, ${name}!`); 
 }
 
 sayHello ("user");
+
+const greeting = () => {
+    if (d.getHours() <= 11) {
+        alert('Good morning user');
+    } else if (d.getHours() < 18) {
+        alert('Good afternoon user');
+    } else {
+        alert('Good evening user');
+    }
+}
+
+const button = document.getElementById("button");
+button.addEventListener("click", greeting);
